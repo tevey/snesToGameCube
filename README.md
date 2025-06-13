5668cc6c3b10d0ac4f7dce4c3896c78e7db5ef41
Viktor René <viktor.rene@gmail.com>
2025-06-13 13:11:08 +0200

    fix: Update code for better compatibility and speed

--- README.md
+++ README.md
@@ -1,15 +1,36 @@
 # SNES to GameCube adapter
+
 ![Image of adapter](headerImage.jpg)
 Code for building a SNES to GameCube controller adapter.
 Also works with the 8Bitdo [Retro Receiver](http://www.8bitdo.com/retro-receiver-snes/)
 
+## Button Mapping
+
+| SNES Button | GameCube Mapping               | Notes                              |
+| ----------- | ------------------------------ | ---------------------------------- |
+| A           | A                              |
+| B           | B                              |
+| X           | X                              |
+| Y           | Y                              |
+| Select      | Y                              | Like SNES Hori-pad / for GBA games |
+| Start       | Start                          |
+| L           | L (digital), L (analog: 0/255) | Analog: 255 if pressed, 0 if not   |
+| R           | R (digital), R (analog: 0/255) | Analog: 255 if pressed, 0 if not   |
+| L+R+Select  | Z                              |
+| D-Pad Left  | D-Pad Left                     |
+| D-Pad Up    | D-Pad Up                       |
+| D-Pad Right | D-Pad Right                    |
+| D-Pad Down  | D-Pad Down                     |
+
 ## Software Requirements
-- [Arduino IDE](https://www.arduino.cc/en/Main/Software) (tested on 1.8.1)
-- [Arduino Nintendo Library](https://github.com/NicoHood/Nintendo) by NicoHood
+
+- [Arduino IDE](https://www.arduino.cc/en/Main/Software) (tested on 2.3.6)
+- [Arduino Nintendo Library](https://github.com/NicoHood/Nintendo) by NicoHood (tested on 1.4.0)
 
 ## Hardware
 
 ### Parts
+
 - Logic Level Converter 5v <-> 3.3v
 - Arduino Pro Mini (5v 16MHz)
 - USB -> Serial adapter if your Arduino comes without USB.
@@ -18,25 +39,28 @@ Also works with the 8Bitdo [Retro Receiver](http://www.8bitdo.com/retro-receiver
 - Male GameCube controller connector (GameCube extension cable)
 
 ### Wiring
+
 ![Wiring diagram](snesToGameCube_bb.png)
 
 The colors of the wires in the cables may not match between the original controller and the extension cable, below is the mapping for my cables the colors may be different for you.
 
 #### SNES pinout
-Type	| Original Controller  | Extension Cable
---------|-----------|----------------
-5v		| white 	| green
-clock	| yellow 	| yellow
-latch	| orange 	| white
-data	| red 		| red
-gnd		| brown 	| black
+
+| Type  | Original Controller | Extension Cable |
+| ----- | ------------------- | --------------- |
+| 5v    | white               | green           |
+| clock | yellow              | yellow          |
+| latch | orange              | white           |
+| data  | red                 | red             |
+| gnd   | brown               | black           |
 
 #### GameCube Pinout
-Type	| Original Controller 	| Extension Cable
---------|-----------|----------------
-gnd		| Black 	| blue
-gnd		| white 	| red
-gnd		| green 	| blue
-data	| red 		| yellow
-5v		| yellow 	| brown
-3.3v	| blue 		| white
+
+| Type | Original Controller | Extension Cable |
+| ---- | ------------------- | --------------- |
+| gnd  | Black               | blue            |
+| gnd  | white               | red             |
+| gnd  | green               | blue            |
+| data | red                 | yellow          |
+| 5v   | yellow              | brown           |
+| 3.3v | blue                | white           |
--- snesToGameCube.ino
+++ snesToGameCube.ino
@@ -2,6 +2,7 @@
 	Code by Viktor René 2018 (@_tEvEy) https://illusionoftime.com
 	This code can be found at https://github.com/tevey/ProMicroUSBGamepad
 	Uses Arduino Nintendo Library(https://github.com/NicoHood/Nintendo) by NicoHood
+  	Updated by Tisse and Viktor 2025 to work better with both Gamecube games, GBI and original Game Boy player.
 */
 
 /*
@@ -24,7 +25,7 @@ data	| red 		| yellow
 */
 #include "Nintendo.h"
 
-//SNES button mapping
+// SNES button mapping
 const int BUTTON_A = 8;
 const int BUTTON_B = 0;
 const int BUTTON_X = 9;
@@ -38,25 +39,28 @@ const int DPAD_DOWN = 5;
 const int DPAD_LEFT = 6;
 const int DPAD_RIGHT = 7;
 
-//SNES pins
+// SNES pins
 const int SNES_DATA_CLOCK_PIN = 11;
 const int SNES_DATA_LATCH_PIN = 10;
 const int SNES_DATA_SERIAL_PIN = 12;
 
-int lastVal = 0;
+// Array holding SNES buttons states
+int buttons[16];
 
-//Array holding SNES buttons states
-int buttons[12];
+// Assign Polling variables
+long pollInterval;
+long nextPoll;
 
 int GC_DATA_PIN = 8;
-//This makes the controller bidirection data line on pin number8
-CGamecubeConsole GamecubeConsole(GC_DATA_PIN);      //Defines a "Gamecube Console" sending data to the console on pin 8
-Gamecube_Data_t d = defaultGamecubeData;   //Structure for data to be sent to console
+// This makes the controller bidirection data line on pin number8
+CGamecubeConsole GamecubeConsole(GC_DATA_PIN); // Defines a "Gamecube Console" sending data to the console on pin 8
+Gamecube_Data_t d = defaultGamecubeData;	   // Structure for data to be sent to console
 
-//This is needed but you don't need a controller on pin 7
+// This is needed but you don't need a controller on pin 7
 CGamecubeController GamecubeController1(7);
 
-void setup(){
+void setup()
+{
 	// Set SNES_DATA_CLOCK_PIN normally HIGH
 	pinMode(SNES_DATA_CLOCK_PIN, OUTPUT);
 	digitalWrite(SNES_DATA_CLOCK_PIN, HIGH);
@@ -70,48 +74,105 @@ void setup(){
 	digitalWrite(SNES_DATA_SERIAL_PIN, HIGH);
 	pinMode(SNES_DATA_SERIAL_PIN, INPUT);
 
-	 //This is needed to run the code.
+	// Initiate buttons-array
+	buttons[0] = 0;
+	buttons[1] = 0;
+	buttons[2] = 0;
+	buttons[3] = 0;
+	buttons[4] = 0;
+	buttons[5] = 0;
+	buttons[6] = 0;
+	buttons[7] = 0;
+	buttons[8] = 0;
+	buttons[9] = 0;
+	buttons[10] = 0;
+	buttons[11] = 0;
+	// Buttons [12 - 15] are not being used, but we initiate them anyway.
+	buttons[12] = 0;
+	buttons[13] = 0;
+	buttons[14] = 0;
+	buttons[15] = 0;
+
+
+	//Initiate Poll-interval varaiables
+	pollInterval = 6667; // microseconds for 150Hz
+	nextPoll = micros(); // set up nextPoll to be done directly
+
+	// This is needed to run the code.
 	GamecubeController1.read();
+
+	//Send a first response to gamecube directly
+  updateGCReport();
+  while (!GamecubeConsole.write(d)){}
 }
 
-void loop() {
-	readSNES();
+
+void loop()
+{
+	if (micros() >= nextPoll)
+	{
+		readSNES();		
+		updateGCReport(); 
+		nextPoll = micros() + pollInterval;
+	}
+	// Always respond to GameCube as fast as possible
+	while (!GamecubeConsole.write(d)){}
 }
 
-void readSNES() {
+void readSNES()
+{
 	// Latch for 12us
 	digitalWrite(SNES_DATA_LATCH_PIN, HIGH);
 	delayMicroseconds(12);
 	digitalWrite(SNES_DATA_LATCH_PIN, LOW);
 	delayMicroseconds(6);
 
-	// Retrieve button presses from shift register by pulling the clock high for 6us
-	for(int i = 0; i < 16; i++){
+	// Retrieve button presses from shift register by pulsing the clock LOW, reading the data, then setting the clock HIGH.
+	for (int i = 0; i < 16; i++)
+	{
 		digitalWrite(SNES_DATA_CLOCK_PIN, LOW);
 		delayMicroseconds(6);
-		if(i <= 11){
-				buttons[i] = !digitalRead(SNES_DATA_SERIAL_PIN);
-				writeGC();
-		}
+		buttons[i] = !digitalRead(SNES_DATA_SERIAL_PIN);
 		digitalWrite(SNES_DATA_CLOCK_PIN, HIGH);
 		delayMicroseconds(6);
-
 	}
 }
 
-void writeGC() {
-	//sends the complied data to console when console polls for the input
+void updateGCReport()
+{
+	// Sends the complied data to console when console polls for the input
 	d.report.a = buttons[BUTTON_A];
 	d.report.b = buttons[BUTTON_B];
 	d.report.x = buttons[BUTTON_X];
 	d.report.y = buttons[BUTTON_Y];
-	d.report.z = buttons[BUTTON_SELECT];
+	// Map Select-button to Y since this is how the SNES Hori-pad and the GBA-games works
+	if (buttons[BUTTON_Y] == 0)
+	{
+		d.report.y = buttons[BUTTON_SELECT];
+	}
 	d.report.start = buttons[BUTTON_START];
 	d.report.r = buttons[BUTTON_R];
 	d.report.l = buttons[BUTTON_L];
+	// L and R analog require values from 0-255, so send 255 or 0 if button is pressed or unpressed
+	d.report.right = buttons[BUTTON_R] * 255;
+	d.report.left = buttons[BUTTON_L] * 255;
+	// Press L+R+Select for Z
+	if (buttons[BUTTON_SELECT] + buttons[BUTTON_R] + buttons[BUTTON_L] == 3)
+	{
+		d.report.y = 0;
+		d.report.r = 0;
+		d.report.l = 0;
+		d.report.right = 0;
+		d.report.left = 0;
+		d.report.z = 1;
+	}
+	else
+	{
+		d.report.z = 0;
+	}
+
 	d.report.dleft = buttons[DPAD_LEFT];
 	d.report.dup = buttons[DPAD_UP];
 	d.report.dright = buttons[DPAD_RIGHT];
 	d.report.ddown = buttons[DPAD_DOWN];
-	GamecubeConsole.write(d);
 }
